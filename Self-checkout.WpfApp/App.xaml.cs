a57066e43549e8dddf76cd3549b074e67636311f
kot2202 <kot2202@onet.pl>
2022-05-17 16:43:06 +0200

    Added content loading of the pages (without navigation yet)

--- Self-checkout.WpfApp/App.xaml.cs
+++ Self-checkout.WpfApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Self_checkout.WpfApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,15 @@ namespace Self_checkout.WpfApp
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow()
+            {
+                DataContext = new MainViewModel()
+            };
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
     }
 }
---
+++ Self-checkout.WpfApp/ViewModels/WelcomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Self_checkout.WpfApp.ViewModels
+{
+    public class WelcomeViewModel : ViewModelBase
+    {
+        public ICommand NavigateCheckoutCommand { get; }
+    }
+}
