3eaa2585e49a0bbd122a2920cd2e24d0a4663eb6
kot2202 <kot2202@onet.pl>
2022-05-18 18:21:57 +0200

    Added checkout input command
    * Removed not needed xaml code

--- Self-checkout.WpfApp/ViewModels/CheckoutViewModel.cs
+++ Self-checkout.WpfApp/ViewModels/CheckoutViewModel.cs
@@ -10,11 +10,24 @@ namespace Self_checkout.WpfApp.ViewModels
 {
     public class CheckoutViewModel : ViewModelBase
     {
+        private string _screenValue;
+
+        public string ScreenValue
+        {
+            get { return _screenValue; }
+            set {
+                _screenValue = value;
+                OnPropertyChanged();
+                }
+        }
+
         public ICommand NavigatePostPurchaseCommand { get; set; }
+        public ICommand AddNumberCommand { get;}
 
         public CheckoutViewModel(Stores.NavigationStore navigationStore)
         {
             NavigatePostPurchaseCommand = new NavigateCommand<PostPurchaseViewModel>(navigationStore, () => new PostPurchaseViewModel(navigationStore));
+            AddNumberCommand = new AddNumberCommand(this);
         }
     }
 }
