5bbc2861edb3eb31c8924b39d61fe6b528d2c9a4
kot2202 <kot2202@onet.pl>
2022-06-02 12:19:11 +0200

    Created product model and edited xaml
    ** Updated xaml to be able to store current order data
    * Product model will need to store current quantity ( if same product is scanned twice )
    * Property of bitmap that may be filled by usage of converter

---
+++ Self-checkout.WpfApp/Models/ProductModel.cs
@@ -0,0 +1,21 @@
+﻿using Self_checkout.WpfApp.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Self_checkout.WpfApp.Models
+{
+    public class ProductModel : Product
+    {
+        public ProductModel(int q = 1) : base()
+        {
+            CalculatedQuantity = q;
+        }
+
+        public float CalculatedQuantity { get; set; }
+        public BitmapImage convertedImage { get; set; }
+    }
+}
--- Self-checkout.WpfApp/ViewModels/CheckoutViewModel.cs
+++ Self-checkout.WpfApp/ViewModels/CheckoutViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Self_checkout.WpfApp.Commands;
+using Self_checkout.WpfApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,13 @@ namespace Self_checkout.WpfApp.ViewModels
                 }
         }
 
+        private List<ProductModel> _listOfProducts = new List<ProductModel>();
+        public List<ProductModel> ListOfProducts
+        {
+            get {  return _listOfProducts; }
+            set { _listOfProducts = value;}
+        }
+
         public ICommand NavigatePostPurchaseCommand { get; set; }
         public ICommand AddNumberCommand { get; }
         public ICommand UndoNumberCommand { get; }
@@ -39,6 +47,17 @@ namespace Self_checkout.WpfApp.ViewModels
             UndoNumberCommand = new UndoNumberCommand(this);
             ClearCommand = new ClearCommand(this);
             AddProductCommand = new AddProductCommand(this);
+
+
+            // TODO REMOVE THIS AFTER TESTING IS DONE
+            List<ProductModel> testingProducts = new List<ProductModel>()
+            {
+                new ProductModel(){product_name = "name1", CalculatedQuantity = 3, product_price = 2.5m},
+                new ProductModel(){product_name = "name2", CalculatedQuantity = 8, product_price = 99.99m},
+                new ProductModel(){product_name = "name3", CalculatedQuantity = 1, product_price = 1.00m}
+            };
+
+            ListOfProducts = testingProducts;
         }
     }
 }
